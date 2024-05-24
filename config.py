c77adf4e1fc624e45787d7fc22ada4260205a4e2
azero7 <120795461+azero7@users.noreply.github.com>
2024-05-24 14:48:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+icon_families = {
+    'card': {'Container': '♢', 'Leaf': '♤'},
+    'king': {'Container': '♔', 'Leaf': '♚'},
+    'simple': {'Container': '*', 'Leaf': '#'},
+    'heart': {'Container':'დ', 'Leaf': '❤'},
+    'weather': {'Container':'☀', 'Leaf': '❆'}
+}
